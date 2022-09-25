65cefd5e99863af72d4aff7af767109eebfa736d
wwl <1547531839@qq.com>
2022-09-25 21:57:26 +0800

    项目初始化

---
+++ vue.config.js
@@ -0,0 +1,29 @@
+const { defineConfig } = require("@vue/cli-service");
+const path = require("path");
+const resolve = (dir) => path.join(__dirname, dir);
+const AutoImport = require("unplugin-auto-import/webpack");
+const Components = require("unplugin-vue-components/webpack");
+const { ElementPlusResolver } = require("unplugin-vue-components/resolvers");
+
+module.exports = defineConfig({
+  transpileDependencies: true,
+  chainWebpack: (config) => {
+    config.resolve.alias
+      .set("@", resolve("src"))
+      .set("components", resolve("components"))
+      .set("utils", resolve("utils"))
+      .set("views", resolve("views"))
+      .set("store", resolve("store"))
+      .set("router", resolve("router"));
+  },
+  configureWebpack: {
+    plugins: [
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+    ],
+  },
+});
