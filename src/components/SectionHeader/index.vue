b53ed3a028c8c3c4f693adb85614eee633e8e813
wwl <1547531839@qq.com>
2022-09-26 23:14:19 +0800

    添加eslint以及区域头部封装

---
+++ src/components/SectionHeader/index.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="section-header">
+		<div class="header-left">
+      <slot name="left"></slot>
+    </div>
+		<div class="header-right">
+      <slot name="right"></slot>
+    </div>
+	</div>
+</template>
+
+<script setup>
+const name = '123'
+console.log(name)
+</script>
+
+<style lang="less" scoped>
+  .section-header {
+    height: 40px;
+    display: flex;
+    justify-content: space-between;
+    border-bottom: 2px solid #c20c0c;
+    align-items: center;
+  }
+</style>
